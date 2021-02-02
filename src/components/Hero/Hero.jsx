b155bc9b790dd3d773238fa7792c290a3c5905f4
rashad <rashadmad@gmail.com>
2021-02-02 13:42:24 -0600

    create see work button

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -44,13 +44,20 @@ const Header = () => {
         <Fade left={isDesktop} bottom={isMobile} duration={1000} delay={1000} distance="30px">
           <Row>
             <Col xs={12}>
-              <p className="hero-cta mt-5">
-                <span className="cta-btn cta-btn--hero">
-                  <Link to="about" smooth duration={1000}>
-                    {cta || 'Learn more'}
-                  </Link>
-                </span>
-              </p>
+              <Row>
+                <p className="hero-cta mt-5">
+                  <span className="cta-btn cta-btn--hero m-2">
+                    <Link to="about" smooth duration={1000}>
+                      {cta || 'Learn more'}
+                    </Link>
+                  </span>
+                  <span className="cta-btn cta-btn--hero m-2">
+                    <Link to="projects" smooth duration={1000}>
+                      {cta || 'See Work'}
+                    </Link>
+                  </span>
+                </p>
+              </Row>
             </Col>
           </Row>
         </Fade>
